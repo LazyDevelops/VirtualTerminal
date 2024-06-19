c447a6b4117a56df91cf284de3c1a6ad7307bf08
CheeseSand <choijoung1479@gmail.com>
2024-06-19 16:47:39 +0000

    etc

---
+++ check.cs
@@ -0,0 +1,400 @@
+using System;
+using System.Collections.Generic;
+using Tree;
+
+namespace VirtualTerminal {
+    class VirtualTerminal
+    {
+        public struct FileNode{
+            public string name { get; }
+            public byte permission { get; }
+            public string UID { get; }
+            public enum FileType { 
+                F, D 
+            } public FileType fileType { get; }
+            public string? content { get; }
+
+            public FileNode(string name, string UID, byte permission, FileType fileType, string? content = null){
+                this.name = name;
+                this.UID = UID;
+                this.permission = permission;
+                this.fileType = fileType;
+                this.content = content;
+            }
+
+            public override string ToString() {
+                return name;
+            }
+        }
+
+        private Tree<FileNode> root;
+        private Tree<FileNode> pwdNode;
+        private Tree<FileNode> homeNode;
+        
+        private string PWD;
+        private string HOME;
+        private string USER;
+
+        public VirtualTerminal()
+        {
+            USER = "user";
+            PWD = $"/home/{USER}";
+            HOME = $"/home/{USER}";
+
+            root = new Tree<FileNode>(new FileNode("/", "root", 0b111101, FileNode.FileType.D));
+            CreateFile("/", new FileNode("home", "root", 0b111101, FileNode.FileType.D));
+            CreateFile("/", new FileNode("root", "root", 0b111000, FileNode.FileType.D));
+            
+            homeNode = CreateFile("/home", new FileNode(USER, USER, 0b111101, FileNode.FileType.D));
+
+            CreateFile(HOME, new FileNode($"Hello_{USER}.txt", "root", 0b111111, FileNode.FileType.F));
+            
+            pwdNode = FindFile(PWD);
+        }
+
+        public Tree<FileNode> CreateFile(string path, FileNode entry)
+        {
+            string[] directories = path.Split('/');
+            Tree<FileNode> current = root;
+
+            current = FindFile(path);
+
+            Tree<FileNode> newFile = new Tree<FileNode>(entry);
+            current.AppendChildNode(newFile);
+            return newFile;
+        }
+
+        public bool RemoveFile(string path)
+        {
+            string[] directories = path.Split('/');
+            Tree<FileNode> current = root;
+            Tree<FileNode> parents = root;
+
+            current = FindFile(path);
+            if(current.Parents != null){
+                parents = current.Parents;
+            }
+
+            if (current.LeftChild == null)
+            {
+                parents.RemoveChildNode(current);
+                return true;
+            }
+            return false;
+        }
+
+        public Tree<FileNode> FindFile(string path)
+        {
+            string[] directories = path.Split('/');
+            Tree<FileNode> current = root;
+
+            for (int i = 1; i < directories.Length; i++)
+            {
+                foreach (Tree<FileNode> tempNode in current.GetChildren())
+                {
+                    if (tempNode.Data.name == directories[i])
+                    {
+                        current = tempNode;
+                        break;
+                    }
+                }
+            }
+
+            return current;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                DisplayPrompt();
+                string? command = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(command))
+                {
+                    continue;
+                }
+                ProcessCommand(command);
+            }
+        }
+
+        private void DisplayPrompt()
+        {
+            WriteColoredText("\x1b[1muser\x1b[22m", ConsoleColor.Green);
+            WriteColoredText(":", Console.ForegroundColor);
+            // homeDirectory 일시 ~ 표시 코드 짜기
+            // if(!currentDirectory.Equals(homeDirectory))
+            //     WriteColoredText("\x1b[1m~\x1b[22m", ConsoleColor.Blue);
+            // else
+            //     WriteColoredText("\x1b[1m/\x1b[22m", ConsoleColor.Blue);
+            WriteColoredText($"\x1b[1m{PWD}\x1b[22m", ConsoleColor.Blue);
+            WriteColoredText("$ ", Console.ForegroundColor);
+        }
+
+        private void ProcessCommand(string command)
+        {
+            string[] args = command.Split(' ');
+            // foreach(string temp in args){
+            //     Console.WriteLine(temp);
+            // }
+
+            switch (args[0])
+            {
+                // case "cd":
+                //     ExecuteCd(args);
+                //     break;
+                // case "cat":
+                //     ExecuteCat(args);
+                //     break;
+                // case "clear":
+                //     ExecuteClear();
+                //     break;
+                case "exit":
+                    ExecuteExit();
+                    break;
+                // case "help":
+                //     ExecuteHelp();
+                //     break;
+                case "ls":
+                    ExecuteLs(args);
+                    break;
+                // case "mkdir":
+                //     ExecuteMkdir(args);
+                //     break;
+                // case "rm":
+                //     ExecuteRm(args);
+                //     break;
+                // case "rmdir":
+                //     ExecuteRmdir(args);
+                //     break;
+                default:
+                    Console.WriteLine($"Command not found: {command}");
+                    break;
+            }
+        }
+
+        private void ExecuteLs(string[] args)
+        {
+            Dictionary<string, bool> options = new Dictionary<string, bool>{
+                { "l", false },
+            };
+            
+            foreach (string temp in args){
+                // -- 옵션을 위한 코드
+                /*if(temp.Contains("--")){
+                    options[temp.Replace("--", "")] = true;
+                }else */if(temp.Contains("-")){
+                    foreach(char c in temp){
+                        if(c != '-'){
+                            options[temp] = true;
+                        }
+                    }
+                }
+            }
+
+            List<Tree<FileNode>> pwdChildren = pwdNode.GetChildren();
+
+            foreach(Tree<FileNode> temp in pwdChildren){
+                if(options["l"]){
+                    char? type = null;
+
+                    switch(temp.Data.fileType){
+                        case FileNode.FileType.F:
+                            type = '-';
+                            break;
+                        case FileNode.FileType.D:
+                            type = 'd';
+                            break;
+                        default:
+                            break;
+                    }
+
+                    string permissions = ConvertPermissionsToString(temp.Data.permission);
+
+                    Console.WriteLine($"{type}{permissions} {temp.Data.UID} {temp.Data.name}");
+                }else{
+                    Console.WriteLine(temp.Data.name);
+                }
+            }
+        }
+
+        private void ExecuteCd(string[] args)
+        {
+            
+            // if (dir == "." || dir == "./")
+            // {
+            //     return;
+            // }
+            // else if (dir == ".." || dir == "../")
+            // {
+            //     if (PWD != "/")
+            //     {
+            //         int lastSlashIndex = PWD.LastIndexOf('/');
+            //         if (lastSlashIndex == 0)
+            //         {
+            //             PWD = "/";
+            //         }
+            //         else
+            //         {
+            //             PWD = PWD.Substring(0, lastSlashIndex);
+            //         }
+            //     }
+            // }
+            // else
+            // {
+            //     string newDir = PWD == "/" ? $"/{dir}" : $"{PWD}/{dir}";
+            //     if (fileSystem.Exists(entry => entry.Path == newDir && entry.IsDirectory))
+            //     {
+            //         PWD = newDir;
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine($"Directory not found: {dir}");
+            //     }
+            // }
+        }
+
+        // private void ExecuteCat(string[] args)
+        // {
+        //     string path = PWD == "/" ? $"/{file}" : $"{PWD}/{file}";
+        //     var entry = fileSystem.Find(entry => entry.Path == path);
+
+        //     if(entry.IsDirectory){
+        //         Console.WriteLine($"Not a file: {file}");
+        //     }
+        //     else if (entry.Path != null)
+        //     {
+        //         Console.WriteLine(entry.Content);
+        //     }
+        //     else
+        //     {
+        //         Console.WriteLine($"File not found: {file}. Creating new file. Enter content (end with a single dot on a line):");
+        //         string content = ReadMultiLineInput();
+        //         fileSystem.Add(new FileSystemEntry(path, USER, 0b110100, 0, content));
+        //         Console.WriteLine("File created.");
+        //     }
+        // }
+
+        // private void ExecuteClear()
+        // {
+        //     Console.Clear();
+        // }
+
+        // private void ExecuteMkdir(string[] args)
+        // {
+        //     string dirPath = PWD == "/" ? $"/{dir}" : $"{PWD}/{dir}";
+        //     if (fileSystem.Exists(entry => entry.Path == dirPath))
+        //     {
+        //         Console.WriteLine($"Directory already exists: {dir}");
+        //     }
+        //     else
+        //     {
+        //         fileSystem.Add(new FileSystemEntry(dirPath, USER, 0b111101, 1));
+        //         Console.WriteLine($"Directory created: {dir}");
+        //     }
+        // }
+
+        // private void ExecuteRmdir(string[] args)
+        // {
+        //     string path = PWD == "/" ? $"/{dir}" : $"{PWD}/{dir}";
+        //     var entry = fileSystem.Find(entry => entry.Path == path);
+
+        //     if (entry.Path == null)
+        //     {
+        //         Console.WriteLine($"Directory not found: {dir}");
+        //     }
+        //     else if (!entry.IsDirectory)
+        //     {
+        //         Console.WriteLine($"Not a directory: {dir}");
+        //     }
+        //     else
+        //     {
+        //         if (IsDirectoryEmpty(path))
+        //         {
+        //             fileSystem.RemoveAll(entry => entry.Path == path);
+        //             Console.WriteLine($"Directory removed: {dir}");
+        //         }
+        //         else
+        //         {
+        //             Console.WriteLine($"Directory not empty: {dir}");
+        //         }
+        //     }
+        // }
+
+        // private void ExecuteRm(string[] args)
+        // {
+        //     string path = PWD == "/" ? $"/{file}" : $"{PWD}/{file}";
+        //     var entry = fileSystem.Find(entry => entry.Path == path);
+
+        //     if (entry.Path == null)
+        //     {
+        //         Console.WriteLine($"File not found: {file}");
+        //     }
+        //     else if (entry.IsDirectory)
+        //     {
+        //         Console.WriteLine($"Not a file: {file}");
+        //     }
+        //     else
+        //     {
+        //         fileSystem.RemoveAll(entry => entry.Path == path);
+        //         Console.WriteLine($"File removed: {file}");
+        //     }
+        // }
+
+        private void ExecuteExit()
+        {
+            Environment.Exit(0);
+        }
+
+        // private void ExecuteHelp()
+        // {
+        //     Console.WriteLine("Available commands:");
+        //     Console.WriteLine("ls - List directory contents");
+        //     Console.WriteLine("cd - Change the current directory");
+        //     Console.WriteLine("cat - Display file content");
+        //     Console.WriteLine("clear - Clear the screen");
+        //     Console.WriteLine("mkdir - Create a new directory");
+        //     Console.WriteLine("rmdir - Remove a directory");
+        //     Console.WriteLine("rm - Remove a file");
+        //     Console.WriteLine("exit - Exit the terminal");
+        // }
+
+        private string ConvertPermissionsToString(short permissions)
+        {
+            string result = string.Empty;
+            result += (permissions & 040) != 0 ? "r" : "-";
+            result += (permissions & 020) != 0 ? "w" : "-";
+            result += (permissions & 010) != 0 ? "x" : "-";
+            result += (permissions & 004) != 0 ? "r" : "-";
+            result += (permissions & 002) != 0 ? "w" : "-";
+            result += (permissions & 001) != 0 ? "x" : "-";
+            return result;
+        }
+
+        private string ReadMultiLineInput()
+        {
+            string content = "";
+            string? line;
+            while ((line = Console.ReadLine()) != ".")
+            {
+                content += line + Environment.NewLine;
+            }
+            return content.TrimEnd('\n');
+        }
+
+        private static void WriteColoredText(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        VirtualTerminal.VirtualTerminal terminal = new VirtualTerminal.VirtualTerminal();
+        terminal.Run();
+    }
+}
--- obj/Debug/net8.0/App.AssemblyInfo.cs
+++ obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbab7a05a4f8cf5e5d2619b5426379d71af73a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f512108541f4e1f61db63eb35d98cb6d730c9a7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
