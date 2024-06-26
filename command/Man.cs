bc1d119910321dc0c2fcb99410d84db679102dea
CheeseSand <choijoung1479@gmail.com>
2024-06-26 06:44:36 +0000

    etc

--- command/Man.cs
+++ command/Man.cs
@@ -4,14 +4,14 @@
     {
         public void Execute(string[] args, VirtualTerminal VT)
         {
-            //if (VT.commandMap.TryGetValue(args[1], out var action))
-            //{
-            //    Console.Write(action.Description());
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"{args[0]}: {arg}: 명렁어를 찾을 수 없습니다.");
-            //}
+            if (VT.commandMap.TryGetValue(args[1], out var action))
+            {
+                Console.Write(action.Description());
+            }
+            else
+            {
+                Console.WriteLine($"{args[0]}: {args[1]}: 명렁어를 찾을 수 없습니다.");
+            }
         }
 
         public string Description()
