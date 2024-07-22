4dc2ab5b426b305c68c0ab669cf6bf9a51861aac
CheeseSand <choijoung1479@gmail.com>
2024-07-23 02:06:52 +0900

    perf: 필요없는 코드 제거
    나중에 GetAbsolutePath파일과 NormalizePath파일 합체 예정

--- FileSystem/GetAbsolutePath.cs
+++ FileSystem/GetAbsolutePath.cs
@@ -10,28 +10,10 @@
                 return NormalizePath(path);
             }
 
-            if (path.StartsWith("~/"))
+            if (path.StartsWith('~'))
             {
                 // 홈 디렉터리(~)로 시작하는 경우
-                return NormalizePath(homeDirectory + "/" + path.Remove(0, 2));
-            }
-
-            if (path == "~")
-            {
-                return homeDirectory;
-            }
-
-            if (path == ".")
-            {
-                // 현재 디렉터리를 나타내는 경우
-                return currentDirectory;
-            }
-
-            if (path == "..")
-            {
-                // 부모 디렉터리를 나타내는 경우
-                int lastSlashIndex = currentDirectory.LastIndexOf('/');
-                return lastSlashIndex > 0 ? currentDirectory.Remove(lastSlashIndex) : "/";
+                return NormalizePath(homeDirectory + "/" + path.Remove(0, 1));
             }
 
             // 파일 이름만 주어진 경우, 현재 디렉터리를 기준으로 절대 경로 생성
