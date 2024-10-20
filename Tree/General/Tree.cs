105da50cc056c181800eb777ee9dea313dbd1c84
CheeseSand <choijoung1479@gmail.com>
2024-10-20 18:51:29 +0900

    feat: 일반 트리 코드 추가를 위한 파일 생성

---
+++ Tree/General/Tree.cs
@@ -0,0 +1,16 @@
+﻿// 트리 코드 삽입 예정
+namespace VirtualTerminal.Tree.General
+{
+    public class Node<T>
+    {
+        public T? Data;
+        public Node<T>? Parent;
+        public List<Node<T>> Children;
+
+        public Node(T? data)
+        {
+            Data = data;
+            Parent = null;
+        }
+    }
+}
