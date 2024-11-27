eedef69d4ece137c02077bc6ce783635a2efd9a2
CheeseSand <choijoung1479@gmail.com>
2024-11-28 00:04:40 +0900

    feat: detail 설명 추가

--- Command/MkDir.cs
+++ Command/MkDir.cs
@@ -64,7 +64,18 @@ namespace VirtualTerminal.Command
         {
             if (detail)
             {
-                return "mkdir - 디렉터리 생성\n";
+                return "\u001b[1m간략한 설명\x1b[22m\n" +
+                       "   mkdir - 디렉터리 생성\n\n" +
+                       "\u001b[1m사용법\u001b[22m\n" +
+                       "   mkdir 폴더명\n\n" +
+                       "\u001b[1m설명\u001b[22m\n" +
+                       "   위에 사용법을 이용하여 디렉터리를 생성할 수 있습니다.\n" +
+                       "   (자세한 사용법은 예시 참조)\n\n" +
+                       "\u001b[1m옵션\u001b[22m\n" +
+                       "   (없음)\n\n" +
+                       "\u001b[1m예시\u001b[22m\n" +
+                       "   mkdir test\n" +
+                       "   mkdir test/helloworld\n";
             }
 
             return "mkdir - 디렉터리 생성";
--- Command/RmDir.cs
+++ Command/RmDir.cs
@@ -58,10 +58,23 @@ namespace VirtualTerminal.Command
         {
             if (detail)
             {
-                return "rmdir - 비어있는 디렉터리를 삭제\n";
+                return "\u001b[1m간략한 설명\x1b[22m\n" +
+                       "   mkdir - 디렉터리 삭제\n\n" +
+                       "\u001b[1m사용법\u001b[22m\n" +
+                       "   mkdir [옵션] 폴더명\n\n" +
+                       "\u001b[1m설명\u001b[22m\n" +
+                       "   위에 사용법을 이용하여 빈 디렉터리를 삭제할 수 있습니다.\n" +
+                       "   -f 없이는 빈 디렉터리가 아닐 경우 삭제가 불가능합니다.\n" +
+                       "   (자세한 사용법은 예시 참조)\n\n" +
+                       "\u001b[1m옵션\u001b[22m\n" +
+                       "   -f\n" +
+                       "       안에 파일이 있어서 삭제합니다.\n" +
+                       "\u001b[1m예시\u001b[22m\n" +
+                       "   mkdir test\n" +
+                       "   mkdir test/helloworld\n";
             }
 
-            return "rmdir - 비어있는 디렉터리를 삭제";
+            return "rmdir - 디렉터리 삭제";
         }
     }
 }
